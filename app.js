81baf4807d063cd5f29b43fd35711ade3cd806fe
James Brock <jbrock3852@gmail.com>
2019-12-03 16:00:47 -0600

    broken db code

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+
+const db = mysql.createConnection ({
+  host: 'cleanearthsoftware.com',
+  port: '3306',
+  user: 'cims_user2234',
+  password: 'CSC4610-01',
+  database: '4236paulgarlandclean_cims2'
+});
+
+// connect to database
+// db.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   }
+//   console.log('Connected to database');
+// });
+//console.log("OUtside test:")
+//console.log(db)
+global.db = db;
+global.db_connected = false
 
 var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api');
+// var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -20,7 +42,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function(){
@@ -12,8 +13,61 @@ describe('GET /', function() {
         request.get('/')
             .expect(200)
             .end(function(err, res) {
-                expect(res.body).to.have.lengthOf(2);
                 done(err);
             });
     });
+});
+
+describe('GET /reports', function() {
+  it('goes to reports page', function(done) {
+      request.get('/reports')
+          .expect(200)
+          .end(function(err, res) {
+              done(err);
+          });
+  });
+});
+
+describe('GET /login', function() {
+  it('goes to login page', function(done) {
+      request.get('/login')
+          .expect(200)
+          .end(function(err, res) {
+              done(err);
+          });
+  });
+});
+
+describe('GET /mainpage', function() {
+  it('goes to mainpage page', function(done) {
+      request.get('/mainpage')
+          .expect(200)
+          .end(function(err, res) {
+              done(err);
+          });
+  });
+});
+
+describe('GET /remove', function() {
+  it('goes to remove page', function(done) {
+      request.get('/remove')
+          .expect(200)
+          .end(function(err, res) {
+              done(err);
+          });
+  });
+});
+
+describe('Database Connection', function() {
+  it('connects to the database', function(done) {
+    db.connect(function(err) {
+      if (err) throw err;
+      db.query("SELECT 1 + 1 AS sum", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        done();
+      });
+    });
+    db.end();
+  });
 });
